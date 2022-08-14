395696c9320274fe4e57d470c5b4c2c06b347437
Pamela Calderon <pamela.calderon.canchanya@gmail.com>
2022-08-14 00:33:43 -0500

    funcion register

--- src/view/Register.js
+++ src/view/Register.js
@@ -1,6 +1,6 @@
-import {createUser} from '../lib/index.js'
+import { createUser, sendEmail} from '../lib/index.js'
 export default () => {
-    const viewRegister = `
+  const viewRegister = `
     <section class="contenido_container2" id="container2">
     <div class = "form">
     <h2 class =" titulo-register">Crear cuenta</h2>
@@ -13,40 +13,50 @@ export default () => {
     <button class="button-google" id="optiongoogle">
         Iniciar Sesión con Google
     </button>
+    <p id="message-error"></p>
     </div>
     </section>
     `
 
-    const divElement = document.createElement('div')
-  
-    divElement.innerHTML = viewRegister;
-   
+  const divElement = document.createElement('div')
 
-    return divElement;
+  divElement.innerHTML = viewRegister;
+
+
+  return divElement;
 }
 
 export const registerFunctions = () => {
-    const btnRegister = document.getElementById('register')
-    btnRegister.addEventListener('click', (e) => {
-        console.log('hola registrandose')
-        const user = document.getElementById('email').value
-        const password = document.getElementById('password').value
-        console.log(user, password)
-        createUser(user, password)
-          .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            window.location.hash ="#/Post"
-            // ...
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(error.code, error.message)
-            // ..
-          });
-        
-    })
+  const btnRegister = document.getElementById('register')
+  btnRegister.addEventListener('click', (e) => {
+    console.log('hola registrandose')
+    const user = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const msgError = document.getElementById('message-error')
+    if (user !== '' && password !== '') {
+      createUser(user, password).then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(userCredential)
+        sendEmail().then(() => {
+          alert("se envio correo")
+        }).catch((error) => {
+          console.log(error)
+        })
+        // window.location.hash ="#/Post"
+        // ...
+      })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(error.code, error.message)
+          // ..
+        });
 
+    } else {
+      msgError.innerHTML = "ingresa email o password faltante"
+    }
+  })
 }
+
 
