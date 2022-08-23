8ce40d62dd8686e035c874b8ba122102ad812064
Pamela Calderon <pamela.calderon.canchanya@gmail.com>
2022-08-22 19:06:43 -0500

    html-register modificado

--- src/view/Register.js
+++ src/view/Register.js
@@ -2,22 +2,36 @@ import { createUser, sendEmail} from '../lib/index.js'
 
 export default () => {
   const viewRegister = `
-    <section class="contenido_container2" id="container2">
-    <div class = "form">
-    <h2 class =" titulo-register">Crear cuenta</h2>
-    <input type="text" id="name" class ="form-input" placeholder ="Nombre de Usuario">
-    <input type="text" id="email" class ="form-input" placeholder ="Correo Electrónico">
-    <input type="text" id="password" class ="form-input" placeholder ="Contraseña">
-    <button class="button-register" id="register">
-        Registrate
-    </button>
-    <button class="button-google" id="optiongoogle">
-        Iniciar Sesión con Google
-    </button>
-    <p id="message-error"></p>
-    </div>
-    </section>
-    `
+  <section class ="contenido_container2" id="container2">
+  <div class = "register-page">
+  <div class = "img-izquierda"></div>
+  <div class = "img-derecha">
+  <div class = button1>
+  <h2 class =" titulo-register">Crear cuenta</h2>
+  <div class = "input-box">
+  <input type="text" id="name" class ="form-input" placeholder ="Nombre de Usuario">
+  <i class="ph-user"></i>
+  </div>
+  <div class = "input-box">
+  <input type="text" id="email" class ="form-input" placeholder ="Correo Electrónico">
+  <i class="ph-envelope"></i>
+  </div>
+  <div class = "input-box">
+  <input type="text" id="password" class ="form-input" placeholder ="Contraseña">
+  <i class="ph-eye-closed"></i>
+  </div>
+  <button class="button-register" id="register">
+      Registrate
+  </button>
+  <button class="button-google" id="optiongoogle" ><img id="imgGoogle" src="imagenes/Google-img.png">
+      Iniciar con Google
+  </button>
+  <p id="message-error"></p>
+  </div>
+  </div>
+  </div>
+  </section>
+  `
 
   const divElement = document.createElement('div')
 
