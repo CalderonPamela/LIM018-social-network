c59d42d74b5e83a70bb8822c5661f21e6995dfd3
Pamela Calderon <pamela.calderon.canchanya@gmail.com>
2022-08-22 19:11:13 -0500

    html-access modificado

--- src/view/Acces.js
+++ src/view/Acces.js
@@ -2,23 +2,34 @@ import { signIn } from '../lib/index.js'
 
 export default () => {
   const viewAcces = `
-   <section class="contenido_container2" id="container2">
-   <div class = "acess">
-   <h2 class =" titulo-acsess">Bienvenido</h2>
-   <input type="text" id="email" class ="form-input" placeholder ="Correo Electrónico" value="pvaphwyuskvurpcseb@bvhrs.com">
-   <input type="text" id="password" class ="form-input"placeholder ="Contraseña">
-   <button class="button-acces" id="acces">
-       Iniciar sesión
-   </button>
-   <button class="button-google" id="optiongoogle">
-       Iniciar Sesión con Google
-   </button>
-   <button class="button-register" id="register">
-       Registrate
-   </button>
-   <p id="message-error"></p>
+  <section class="contenido_container2" id="container2">
+  <div class = "access-page">
+  <div class = "img-izquierda"></div>
+  <div class = "img-derecha">
+  <div class = "access">
+  <h2 class =" titulo-access">Bienvenido</h2>
+  <div class = "input-box">
+  <input type="text" id="email" class ="form-input" placeholder ="Correo Electrónico" value="pvaphwyuskvurpcseb@bvhrs.com">
+  <i class="ph-envelope"></i>
    </div>
-   </section>`
+   <div class = "input-box">
+  <input type="text" id="password" class ="form-input"placeholder ="Contraseña">
+  <i class="ph-eye-closed"></i>
+   </div>
+  <button class="button-access" id="acces">
+      Iniciar sesión
+  </button>
+  <button class="button-google" id="optiongoogle"><img id="imgGoogle" src="imagenes/Google-img.png">
+      Iniciar con Google
+  </button>
+  <button class="button-register" id="register">
+      Registrate
+  </button>
+  <p id="message-error"></p>
+  </div>
+  </div>
+  </div>
+  </section>`
 
   const divElement = document.createElement('div')
   divElement.innerHTML = viewAcces;
